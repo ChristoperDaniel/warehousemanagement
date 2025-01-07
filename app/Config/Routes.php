a45bab8612c8b947208d63001cff9828465add42
ChristoperDaniel <christoper.daniel04@gmail.com>
2025-01-07 15:25:00 +0700

    edit routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,12 +8,14 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->get('/product', 'ProductController::index');
+$routes->get('/product/getDataProductOnly', 'ProductController::getDataProductOnly');
 $routes->post('/product/addProduct', 'ProductController::addProduct');
 $routes->post('/product/updateQuantityProduct/(:num)', 'productController::updateQuantityProduct/$1');
 $routes->post('/product/updateRestockStatus/(:num)', 'productController::updateRestockStatus/$1');
 $routes->get('/product/deleteProduct/(:num)', 'productController::deleteProduct/$1');
 
 $routes->get('/employee', 'EmployeeController::index');
+$routes->get('/product/getDataEmployeeOnly', 'EmployeeController::getDataEmployeeOnly');
 $routes->post('/employee/inputDataEmployee', 'EmployeeController::inputDataEmployee');
 $routes->post('/employee/updateEmployee', 'EmployeeController::updateEmployee');
 $routes->post('/employee/deleteEmployee', 'EmployeeController::deleteEmployee');
